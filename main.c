642373b6878e4425202deea0f45298a46eeb2180
maciejspychala <mac.spychala@gmail.com>
2017-05-30 08:46:33 +0200

    add wait function

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <unistd.h>
 #include <stdio.h>
 #include "lamport.h"
+#include <time.h>
+#include <stdlib.h>
+
+void wait_sec(int min, int max) {
+    unsigned int time = (rand() % (max - min)) + min;
+    sleep(time);
+}
 
 int main(int argc, char** argv) {
   return 0;
