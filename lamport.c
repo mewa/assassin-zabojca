4cc937e48023a0a8bbd58828c6fea6fe1bcc0f6c
mchmiel <marcin.k.chmiel@gmail.com>
2017-06-10 09:57:04 +0200

    Added Lamport clock MPI wrappers

---
+++ lamport.c
@@ -0,0 +1,51 @@
+#include "lamport.h"
+#include <unistd.h>
+#include <pthread.h>
+
+const unsigned long MSG_MAX_SIZE = 1024;
+
+pthread_mutex_t clk_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+int lamport_send(void const* data, unsigned long len, MPI_Datatype dtype, int dest,
+		 int tag, MPI_Comm comm,
+		 unsigned long* clock, send_fun send_f) {
+  pthread_mutex_lock(&clk_mutex);
+  
+  int pos = 0;
+  unsigned char send_buf[MSG_MAX_SIZE];
+  (*clock)++;
+  
+  MPI_Pack(clock, 1, MPI_UNSIGNED_LONG, send_buf, MSG_MAX_SIZE, &pos, comm);
+  MPI_Pack(data, len, dtype, send_buf, MSG_MAX_SIZE, &pos, comm);
+  
+  int ret = send_f(send_buf, 1, MPI_PACKED, dest, tag, comm);
+  
+  pthread_mutex_unlock(&clk_mutex);
+  return ret;
+}
+
+int lamport_recv(void* data, unsigned long len, MPI_Datatype dtype, int source,
+		 int tag, MPI_Comm comm, MPI_Status* status,
+		 unsigned long* clock, recv_fun recv_f) {
+  pthread_mutex_lock(&clk_mutex);
+
+  int pos = 0;
+  unsigned char recv_buf[MSG_MAX_SIZE];
+  unsigned long recv_clk;
+
+  int ret = recv_f(recv_buf, 1, MPI_PACKED, source, tag, comm, status);
+  if (ret < 0)
+    goto recv_err;
+  
+  MPI_Unpack(recv_buf, MSG_MAX_SIZE, &pos, &recv_clk, 1, MPI_UNSIGNED_LONG, comm);
+  MPI_Unpack(recv_buf, MSG_MAX_SIZE, &pos, data, len, dtype, comm);
+  
+  *clock = ulmax(*clock + 1, recv_clk);
+ recv_err:
+  pthread_mutex_unlock(&clk_mutex);
+  return ret;
+}
+
+unsigned long ulmax(unsigned long a, unsigned long b) {
+  return a > b ? a : b;
+}
--- main.c
+++ main.c
@@ -1,7 +1,28 @@
 #include <unistd.h>
 #include <stdio.h>
 #include "lamport.h"
+#include <mpi.h>
+
+#define TAG 12
 
 int main(int argc, char** argv) {
+  int d = 1, i;
+  int rank, size;
+  unsigned long clk = 0;
+  MPI_Init(&argc, &argv);
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  MPI_Status status;
+  for (i = 0; i < 10; ++i) {
+    const unsigned int to = (rank + 1) % size;
+    lamport_send(&d, 1, MPI_INT, to, TAG, MPI_COMM_WORLD, &clk, MPI_Send);
+    printf("[%d @ %ld]: sent to [%d]\n", rank, clk, (rank + 1) % size);
+    const unsigned int from = (rank - 1 + size) % size;
+    lamport_recv(&d, 1, MPI_INT, from, TAG, MPI_COMM_WORLD, &status, &clk, MPI_Recv);
+    printf("[%d @ %ld]: received\n", rank, clk);
+  }
+  MPI_Finalize();
   return 0;
 }
