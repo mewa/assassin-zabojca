2cc1ce0be6c92133b2d805c17111a9880b5b092b
maciejspychala <mac.spychala@gmail.com>
2017-06-12 14:12:32 +0000

    lamport send/recv buffer size fix

--- lamport.c
+++ lamport.c
@@ -1,6 +1,7 @@
 #include "lamport.h"
 #include <unistd.h>
 #include <pthread.h>
+#include <stdlib.h>
 
 const unsigned long MSG_MAX_SIZE = 1024;
 
@@ -12,13 +13,13 @@ int lamport_send(void const* data, unsigned long len, MPI_Datatype dtype, int de
   pthread_mutex_lock(&clk_mutex);
   
   int pos = 0;
-  unsigned char send_buf[MSG_MAX_SIZE];
+  void *send_buf = malloc(MSG_MAX_SIZE);
   (*clock)++;
   
   MPI_Pack(clock, 1, MPI_UNSIGNED_LONG, send_buf, MSG_MAX_SIZE, &pos, comm);
   MPI_Pack(data, len, dtype, send_buf, MSG_MAX_SIZE, &pos, comm);
   
-  int ret = send_f(send_buf, 1, MPI_PACKED, dest, tag, comm);
+  int ret = send_f(send_buf, pos, MPI_PACKED, dest, tag, comm);
   
   pthread_mutex_unlock(&clk_mutex);
   return ret;
@@ -30,10 +31,10 @@ int lamport_recv(void* data, unsigned long len, MPI_Datatype dtype, int source,
   pthread_mutex_lock(&clk_mutex);
 
   int pos = 0;
-  unsigned char recv_buf[MSG_MAX_SIZE];
+  void *recv_buf = malloc(MSG_MAX_SIZE);
   unsigned long recv_clk;
 
-  int ret = recv_f(recv_buf, 1, MPI_PACKED, source, tag, comm, status);
+  int ret = recv_f(recv_buf, MSG_MAX_SIZE, MPI_PACKED, source, tag, comm, status);
   if (ret < 0)
     goto recv_err;
   
